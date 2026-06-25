e289fef8d7a3b57d5a08b2c17472d75d5737a3d9
Manika <manika.arora.0404@gmail.com>
2026-06-25 16:10:17 +0530

    commit

--- use of map/script.js
+++ use of map/script.js
@@ -38,8 +38,7 @@ cardContainer.innerHTML=products.map(
     <img src="${product.image}">
     <h2>${product.name}</h2>
     <p>${product.description}</p>
-    <p>${product.price}</p>
-    
+    <h4>${product.price}</h4> 
     </div>
     `
 );
